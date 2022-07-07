8a1d075cb213daf4f42cb1848d51a5741f19cae4
Ерошкин Мирослав <eroshkin_mir.ru@mail.ru>
2022-07-08 01:13:26 +0300

    First commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int miles;
+        int readout = 20;
+        if (cost < 20) {
+                miles = 0;
+ } else {
+            miles = cost / readout;
+        }
+        return miles;
+    }
+}
