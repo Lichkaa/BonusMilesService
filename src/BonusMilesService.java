cf7bfc5dcd815f77e3c66426140505ac3b976609
Lichkaa <105723096+Lichkaa@users.noreply.github.com>
2022-07-20 21:33:54 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int miles;
-        if (cost < 20) {
-                miles = 0;
-        } else {
-            miles = cost / 20;
-        }
-        return miles;
+        int bonusMiles;
+        bonusMiles = cost / 20;
+        return bonusMiles;
     }
 }
